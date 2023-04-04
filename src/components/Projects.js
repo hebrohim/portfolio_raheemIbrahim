6cad7c314ff67f4db65a67df46b99223bd29c926
hebrohim <raheemibrohim@mail.com>
2023-04-04 13:58:36 +0000

    style:edit hide button styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -17,17 +17,18 @@ const Projects = ({project,loading}) => {
         
            
                  <div className="w-[80vw] flex flex-col mb-5 md:w-[30vw] md:mx-2 " key={id}>
-                <h4 className="font-logoFont capitalize text-center font-bold text-2xl mb-2">{name}</h4>
-                <img src= {screenShot} className= "w-full h-96  rounded-t-2xl object-cover"/>
+                <h4 className="font-logoFont capitalize text-center font-bold text-2xl py-2  rounded-t-2xl bg-slate-200">{name}</h4>
+                <img src= {screenShot} className= "w-full h-full  object-fill md:h-72"/>
 {
     showContent?<button onClick={()=>{setshowContent(!showContent)}} className= " bg-amber-600 p-2 text-xl rounded-b-2xl flex justify-center items-center font-semibold">Expand <MdExpandMore/></button>:
-                <div className=" p-5 rounded-b-2xl bg-white">
+    <div>
+                <div className=" p-5 bg-white">
                     <p className="font-semibold">
                         {description}
                     </p>
-                    <button onClick={()=>{setshowContent(!showContent)}} className= "w-full bg-amber-600 p-2 text-xl rounded-md flex items-center mt-4 font-semibold flex justify-center items-center ">Hide <MdExpandLess/></button>
                 </div>
-
+                    <button onClick={()=>{setshowContent(!showContent)}} className= "w-full bg-amber-600 p-2 text-xl rounded-b-2xl flex items-center font-semibold justify-center  ">Hide <MdExpandLess/></button>
+</div>
 }
                 
                 </div>
